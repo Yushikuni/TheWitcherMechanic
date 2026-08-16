acf965d6df40fecbb724f4cb74a19c61c59e0ed0
Kvetuse Husakova <42646031+Yushikuni@users.noreply.github.com>
2026-08-16 17:02:59 +0200

    add new cpp classes for melee enemy and magic enemy. tag magic is only for artefacts

---
+++ Source/Witcher3Mechanic/Public/MageEnemy.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy.h"
+#include "MageEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WITCHER3MECHANIC_API AMageEnemy : public AEnemy
+{
+	GENERATED_BODY()
+	
+};
